900b630a2c34693d3eac1f22d524b51e344a6f96
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-01-24 17:20:30 +0900

    [Learning] float and double.

--- app/src/main/java/me/nkym/myfirstapp/Basics.kt
+++ app/src/main/java/me/nkym/myfirstapp/Basics.kt
@@ -1,12 +1,19 @@
 package me.nkym.myfirstapp
 
 fun main(){
+    /* Variables */
     val number1 = 1
     var myAge = 20
     myAge = 21
     println(myAge)
 
+    /* Datatype */
     val num1 = 1  //Datatype: Int
     val num2: Byte = 1  //Datatype: Byte
 
+    /* Float and Double */
+    val pi = 3.14
+    val pi2 = 3.14f
+    val pi3 = 3.1415926535f
+    println(pi3)  //3.1415927
 }
