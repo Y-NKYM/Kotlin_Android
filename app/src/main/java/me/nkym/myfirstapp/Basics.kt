3acd61c1a4b935ed037ebf11ad569ac106b9ded8
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-01-24 17:12:21 +0900

    [Learning] val vs var.

--- app/src/main/java/me/nkym/myfirstapp/Basics.kt
+++ app/src/main/java/me/nkym/myfirstapp/Basics.kt
@@ -2,5 +2,10 @@ package me.nkym.myfirstapp
 
 fun main(){
     /* 左のプレイボタンクリックにてコンソール内にHello Worldの文字が表示される。 */
-    println("Hello World!")
+    val number1 = 1
+    // number1 = 2 //valは変更を良しとしないため、エラーとなる。
+
+    var myAge = 20
+    myAge = 21
+    println(myAge)
 }
