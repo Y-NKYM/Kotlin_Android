4b232ce8ee14a27dc6f22986c4cece02776c550f
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-01-24 17:15:24 +0900

    [Learning] setting data types.

--- app/src/main/java/me/nkym/myfirstapp/Basics.kt
+++ app/src/main/java/me/nkym/myfirstapp/Basics.kt
@@ -1,11 +1,12 @@
 package me.nkym.myfirstapp
 
 fun main(){
-    /* 左のプレイボタンクリックにてコンソール内にHello Worldの文字が表示される。 */
     val number1 = 1
-    // number1 = 2 //valは変更を良しとしないため、エラーとなる。
-
     var myAge = 20
     myAge = 21
     println(myAge)
+
+    val num1 = 1  //Datatype: Int
+    val num2: Byte = 1  //Datatype: Byte
+
 }
