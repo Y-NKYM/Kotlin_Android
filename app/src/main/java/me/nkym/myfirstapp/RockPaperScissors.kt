e16cb262c4645192f49c7a418cb0ef4d3b47a0ac
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-01-25 20:52:03 +0900

    [Update] Replacing if statement to when statement.

--- app/src/main/java/me/nkym/myfirstapp/RockPaperScissors.kt
+++ app/src/main/java/me/nkym/myfirstapp/RockPaperScissors.kt
@@ -22,12 +22,16 @@ fun main(){
 
     /* コンピューター側の手をランダムで決める */
     val randomNumber = (1..3).random()
-    if(randomNumber == 1){
-        computerChoice = "Rock"
-    } else if(randomNumber == 2) {
-        computerChoice = "Paper"
-    } else if(randomNumber == 3) {
-        computerChoice = "Scissors"
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissors"
+        }
     }
     println(computerChoice)
 
