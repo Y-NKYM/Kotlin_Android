1951b96adcc19eaf05deb4a4573ff979352a1c7e
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-01-27 14:59:26 +0900

    [Learning] Multiple parameters in function.

--- app/src/main/java/me/nkym/myfirstapp/Basics.kt
+++ app/src/main/java/me/nkym/myfirstapp/Basics.kt
@@ -100,15 +100,17 @@ fun main(){
     */
 
     /* Function */
-    fun makeCoffee(sugarCount: Int){
+    fun makeCoffee(sugarCount: Int, name: String){
         if(sugarCount == 1){
-            println("Coffee with $sugarCount spoon of sugar")
+            println("Coffee with $sugarCount spoon of sugar for $name")
+        } else if(sugarCount == 0) {
+            println("Coffee with no sugar for $name")
         } else {
-            println("Coffee with $sugarCount spoons of sugar")
+            println("Coffee with $sugarCount spoons of sugar for $name")
         }
 
     }
-    makeCoffee(1)
-    makeCoffee(5)
+    makeCoffee(1, "Jimmy")
+    makeCoffee(5, "Henry")
 
 }
