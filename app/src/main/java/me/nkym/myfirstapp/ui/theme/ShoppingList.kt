27581f2e6e4f2ae29e8a8552293b8aaa7a6dd66a
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-02-07 20:57:50 +0900

    [Learning] Setting up alert pop ups.

--- app/src/main/java/me/nkym/myfirstapp/ui/theme/ShoppingList.kt
+++ app/src/main/java/me/nkym/myfirstapp/ui/theme/ShoppingList.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,23 +26,41 @@ data class ShoppingItem(
     var isEditing:Boolean = false
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(){
+    //アイテムリスト
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    //ポップアップ表示有無
+    var showDialog by remember { mutableStateOf(false) }
+
+
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { showDialog = true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text("Add Item")
         }
         LazyColumn(
-            modifier = Modifier.fillMaxSize().padding(16.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
             items(sItems) {}
         }
+    }
+
+    if(showDialog){
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            confirmButton = { /*TODO*/ },
+            title = { Text("THIS IS AN ALERT") },
+            text = { Text("Hello World") }
+        )
     }
 }
