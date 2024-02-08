9e6e7c847fbf72e90043df59ac1936e2cd811a9e
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-02-08 22:00:52 +0900

    [Add] View and layout items when add button clicked.

--- app/src/main/java/me/nkym/myfirstapp/ui/theme/ShoppingList.kt
+++ app/src/main/java/me/nkym/myfirstapp/ui/theme/ShoppingList.kt
@@ -1,5 +1,7 @@
 package me.nkym.myfirstapp.ui.theme
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -56,7 +60,9 @@ fun ShoppingListApp(){
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            items(sItems) {}
+            items(sItems) {
+                ShoppingListItem(it, {}, {})
+            }
         }
     }
 
@@ -86,7 +92,7 @@ fun ShoppingListApp(){
                                     Text(text = "Add")
                                 }
                                 Button(onClick = {
-
+                                    showDialog = false
                                 }) {
                                     Text(text = "Cancel")
                                 }
@@ -115,5 +121,25 @@ fun ShoppingListApp(){
                 }
             }
         )
+    }
+}
+
+//アイテム一つ一つのレイアウト設定用関数
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick : () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color(0xFF000000)),
+                shape = RoundedCornerShape(20)
+            )
+    ){
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
